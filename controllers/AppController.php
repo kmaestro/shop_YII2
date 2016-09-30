b1491a63536dc8d9cf02b2e8a8f18190a918da57
Дима <polishuk1003@gmail.com>
2016-09-30 23:45:44 +0300

    сделал контент для категории
    сделал контент для категории

---
+++ controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polis
+ * Date: 28.09.2016
+ * Time: 21:35
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+
+class AppController extends Controller
+{
+
+}
