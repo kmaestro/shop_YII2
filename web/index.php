ad2a7338de05c49099e90611377636d893b7c2cb
maestro881 <polishuk1003@gmail.com>
2016-10-11 14:08:48 +0300

    Сделал контролер и модель корзины для товара

--- web/index.php
+++ web/index.php
@@ -11,7 +11,3 @@ $config = require(__DIR__ . '/../config/web.php');
 require_once __DIR__ . '/../functions.php';
 
 (new yii\web\Application($config))->run();
-
-$time = (microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) * 1000;
-$time = substr($time, 0, 5);
-echo "<div style='position: fixed; bottom: 10px'> $time ms</div>";
